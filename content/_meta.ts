2831473076028620643df4f15f532f606b954a5f
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-11 18:06:09 -0800

    Update content/_meta.ts
    Updated via Jolli repository editor

--- content/_meta.ts
+++ content/_meta.ts
@@ -4,7 +4,7 @@ export default {
     title: 'API Reference',
     type: 'menu',
     items: {
-      'valid-jolli-api-yaml': { title: 'Valid - Jolli API (YAML)', href: '/api-docs/valid-jolli-api-yaml' },
+      'valid-jolli-api-yaml': { title: 'Custom 1', href: '/api-docs/valid-jolli-api-yaml' },
       'valid-pet-store-api-yaml': { title: 'Valid - Pet Store API (YAML)', href: '/api-docs/valid-pet-store-api-yaml' }
     }
   }
